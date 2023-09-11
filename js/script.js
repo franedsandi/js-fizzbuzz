bbf26e85db2bf04a503480baead5ecdcb6cfc26e
franedsandi <franedsandi@gmail.com>
2023-09-11 17:14:20 +0200

    [chore]  correction without ! and starting from 1

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 /* create boxes */
-const boxesContainer = document.querySelector('.boxes');
-for (let i = 0; i < 300; i++) {
+const boxesContainer2 = document.querySelector('.boxes');
+for(let i = 1; i <= 100; i++){
   const box = document.createElement('div');
   box.classList.add('box');
-
-  console.log(i, (i + 1) % 3)
+  
+   /* if is fizz and buzz ant the same time *common moltiplier of 3 and 5* = FIZZ BUZZ */
+  if((i % 3 === 0) && (i % 5 === 0)){
+    box.classList.add('bg-fizzbuzz');
+    console.log('Fizz Buzz')
+    box.append('Fizz Buzz');
+  }
+   /* if is multiple of 5 = buzz */
+  else if(i % 5 === 0){
+    box.classList.add('bg-buzz');
+    console.log('Buzz')
+    box.append('Buzz');
+  }
   /* if is multiple of 3 = fizz */
-  if (!((i + 1) % 3)) {
+  else if((i % 3 === 0)){
     box.classList.add('bg-fizz');
     console.log('Fizz')
     box.append('Fizz');
-  /* if is multiple of 5 = buzz */
-  } else if (!((i + 1) % 5)) {
-    box.classList.add('bg-buzz');
-    console.log('buzz')
-    box.append('Buzz');
+  }
   /* everything else = number */
-  } else {
-    box.append(i + 1); // Agregar el número solo si no es un múltiplo de 3 ni de 5
+  else{
+    console.log(i)
+    box.append(i)
   }
-   /* if is fizz and buzz ant the same time *common moltiplier of 3 and 5* = FIZZ BUZZ */
-  if (!((i + 1) % 3) && !((i + 1) % 5)) {
-    box.classList.add('bg-fizzbuzz');
-    console.log('fizz buzz')
-    box.append(' ' + 'Buzz');
-  }
-  boxesContainer.append(box);
+  boxesContainer2.append(box);
 }
